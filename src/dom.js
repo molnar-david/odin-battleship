16d21483145c7480a6ee081947fade4dd32142d5
molnar-david <d.molnarka@hotmail.com>
2024-11-26 17:53:54 +0100

    Refactor initGame function, implement auto-place button

--- src/dom.js
+++ src/dom.js
@@ -7,6 +7,32 @@ function renderAttack(gameboardDiv, x, y) {
     gameboardSquareDivs[x*10+y].classList.add('hit');
 }
 
+function renderGameboards(player, computer) {
+    const playerGameboardDiv = document.getElementById('player-gameboard');
+    const playerGameboardSquareDivs = Array.from(playerGameboardDiv.getElementsByClassName('gameboard-square'));
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (typeof player.gameboard.board[i][j] === 'object') {
+                playerGameboardSquareDivs[i*10+j].classList.add('ship');
+            } else {
+                playerGameboardSquareDivs[i*10+j].classList.remove('ship');
+            }
+        }
+    }
+    
+    const computerGameboardDiv = document.getElementById('computer-gameboard');
+    const computerGameboardSquareDivs = Array.from(computerGameboardDiv.getElementsByClassName('gameboard-square'));
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (typeof computer.gameboard.board[i][j] === 'object') {
+                computerGameboardSquareDivs[i*10+j].classList.add('ship');
+            } else {
+                computerGameboardSquareDivs[i*10+j].classList.remove('ship');
+            }
+        }
+    }
+}
+
 function initGameboards(player, computer) {
     const playerGameboardDiv = document.getElementById('player-gameboard');
     for (let i = 0; i < 10; i++) {
@@ -42,28 +68,40 @@ function initGameboards(player, computer) {
             computerGameboardDiv.appendChild(gameboardSquareDiv);
         }
     }
+
+    placeShipRandomly(computer, 5);
+    placeShipRandomly(computer, 4);
+    placeShipRandomly(computer, 3);
+    placeShipRandomly(computer, 3);
+    placeShipRandomly(computer, 2);
+    renderGameboards(player, computer);
 }
 
-function renderGameboards(player, computer) {
-    const playerGameboardDiv = document.getElementById('player-gameboard');
-    const playerGameboardSquareDivs = Array.from(playerGameboardDiv.getElementsByClassName('gameboard-square'));
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            if (typeof player.gameboard.board[i][j] === 'object') {
-                playerGameboardSquareDivs[i*10+j].classList.add('ship');
-            }
-        }
-    }
-    
-    const computerGameboardDiv = document.getElementById('computer-gameboard');
-    const computerGameboardSquareDivs = Array.from(computerGameboardDiv.getElementsByClassName('gameboard-square'));
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            if (typeof computer.gameboard.board[i][j] === 'object') {
-                computerGameboardSquareDivs[i*10+j].classList.add('ship');
-            }
-        }
+function placeShipRandomly(player, shipLength) {
+    let isValidPlacement = false;
+    while (!isValidPlacement) {
+        const x = Math.floor(Math.random() * (10 - shipLength));
+        const y = Math.floor(Math.random() * 10);
+        const clone = player.gameboard.clone();
+        const coords = clone.placeShip(x, y, shipLength);
+
+        const isOutOfBounds = !Boolean(coords.length);
+        const isTaken = coords.some((coord) => typeof player.gameboard.board[coord[0]][coord[1]] === 'object');
+        if (isValidPlacement = !isOutOfBounds && !isTaken) player.gameboard.placeShip(x, y, shipLength);
     }
 }
 
-module.exports = { initGameboards, renderGameboards };
+function initBtns(player, computer) {
+    const autoPlaceBtn = document.getElementById('auto-place-btn');
+    autoPlaceBtn.addEventListener('click', () => {
+        const clone = player.clone();
+        placeShipRandomly(clone, 5);
+        placeShipRandomly(clone, 4);
+        placeShipRandomly(clone, 3);
+        placeShipRandomly(clone, 3);
+        placeShipRandomly(clone, 2);
+        renderGameboards(clone, computer);
+    });
+}
+
+module.exports = { initGameboards, initBtns };
--- src/game.js
+++ src/game.js
@@ -1,37 +1,11 @@
 const Player = require('./Player');
-const { initGameboards, renderGameboards } = require('./dom.js');
-
-function placeShipRandomly(player, shipLength) {
-    let isValidPlacement = false;
-    while (!isValidPlacement) {
-        const x = Math.floor(Math.random() * (10 - shipLength));
-        const y = Math.floor(Math.random() * 10);
-        const clone = player.gameboard.clone();
-        const coords = clone.placeShip(x, y, shipLength);
-
-        const isOutOfBounds = !Boolean(coords.length);
-        const isTaken = coords.some((coord) => typeof player.gameboard.board[coord[0]][coord[1]] === 'object');
-        if (isValidPlacement = !isOutOfBounds && !isTaken) player.gameboard.placeShip(x, y, shipLength);
-    }
-}
+const { initGameboards, initBtns } = require('./dom.js');
 
 function initGame() {
     const player = new Player('Player');
     const computer = new Player();
+    initBtns(player, computer);
     initGameboards(player, computer);
-
-    placeShipRandomly(player, 5);
-    placeShipRandomly(player, 4);
-    placeShipRandomly(player, 3);
-    placeShipRandomly(player, 3);
-    placeShipRandomly(player, 2);
-    
-    placeShipRandomly(computer, 5);
-    placeShipRandomly(computer, 4);
-    placeShipRandomly(computer, 3);
-    placeShipRandomly(computer, 3);
-    placeShipRandomly(computer, 2);
-    renderGameboards(player, computer);
 }
 
 module.exports = initGame;
